309c1839dd4248a635e096a3151e780095511acd
monrold <soporte.draw@gmail.com>
2024-11-20 16:58:45 -0600

    fix: search console text

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://stroitsurfaces.com", // URL base de tu sitio
+  base: "/", // Raíz del dominio
   integrations: [tailwind()]
 });
