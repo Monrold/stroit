46727bced1349ab20cae19fdec0b34f81ba5613e
Monrold <soporte.draw@gmail.com>
2025-03-08 21:29:56 -0600

    feat: post hog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://stroitsurfaces.com", // URL base de tu sitio
   base: "/", // Raíz del dominio
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()]
 });
